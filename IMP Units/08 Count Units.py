5310adb72608a37007a96bc756921e9fecf06118
Pablo Impallari <impallari@gmail.com>
2014-07-08 11:19:07 -0300

    Adding Count Units macro
    Count (user defined) units in selected glyphs.

---
+++ IMP Units/08 Count Units.py
@@ -0,0 +1,49 @@
+#FLM: Count Units in selected glyph
+
+# Description:
+# Count Units in selected glyph
+
+# Credits:
+# Pablo Impallari
+# http://www.impallari.com
+
+unit = 22.0 # Change this value to your desired unit size
+
+# Clear Output windows
+from FL import *
+fl.output=""
+
+from robofab.world import CurrentFont
+
+f = CurrentFont()
+list = f.selection
+# list.sort()
+
+biggest = 0
+sumatoria = 0
+
+for g in list:	
+	
+	glyph = f[g].name
+	ancho = f[g].width
+	units = ancho / unit
+	print "%s: %s units" % (glyph, units)
+	
+	if units > biggest:
+		biggest = units
+	
+	sumatoria = sumatoria + units
+
+promedio = sumatoria / len(list)	
+
+print ""
+print "Average is %s units" % (promedio)		
+print "Wider is %s units" % (biggest)
+
+
+
+
+
+
+
+
